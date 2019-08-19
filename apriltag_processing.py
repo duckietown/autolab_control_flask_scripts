894722cf55f6c2fa2fdf6511cb00809ca3024b97
AmaurX <amaury.camus92@gmail.com>
2019-08-19 10:38:24 +0200

    more change

--- apriltag_processing.py
+++ apriltag_processing.py
@@ -2,7 +2,7 @@ import subprocess
 import rosbag
 
 
-def start_apriltag_processing(input_bag_path, output_bag_path):
+def start_apriltag_processing(input_bag_path, output_bag_path, mount_computer_side, mount_container_side="/data"):
     bag = rosbag.Bag(input_bag_path)
     watchtowers = []
     for topic, _, _ in bag.read_messages():
@@ -12,8 +12,8 @@ def start_apriltag_processing(input_bag_path, output_bag_path):
                 if part not in watchtowers:
                     watchtowers.append(part)
     for watchtower_id in watchtowers:
-        cmd = "docker-compose -f apriltag_processor_compose.yaml run -d -e ACQ_DEVICE_NAME=%s -e INPUT_BAG_PATH=%s -e OUTPUT_BAG_PATH=%s --name apriltagprocessor%s apriltag-processor" % (
-            watchtower_id, input_bag_path, output_bag_path, watchtower_id)
+        cmd = "docker-compose -f apriltag_processor_compose.yaml run -d -v %s:%s -e ACQ_DEVICE_NAME=%s -e INPUT_BAG_PATH=%s -e OUTPUT_BAG_PATH=%s --name apriltagprocessor%s apriltag-processor" % (
+            mount_computer_side, mount_container_side, watchtower_id, input_bag_path, output_bag_path, watchtower_id)
         try:
             res = subprocess.check_output(cmd, shell=True)
             return ("Success: %s" % res)
