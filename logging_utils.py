8d7ec8b98e64e86b82940aa0b3113913b6f6d572
webbe035 <webbe035@github.com>
2019-08-19 14:29:52 +0200

    minor

--- logging_utils.py
+++ logging_utils.py
@@ -1,7 +1,7 @@
 import subprocess,time
 
 def start_logging(computer, filename, device_list, submission_id, step_name):
-    cmd = "docker -H %s rm -f bag_recorder || echo not bag_recorder before; cd /home/amaury/AIDO3_experiment_data; mkdir -p submission_%s/%s docker -H %s run --name bag_recorder --rm -v /home/amaury/AIDO3_experiment_data:/data --net=host -dit duckietown/dt-ros-commons:master19-amd64 /bin/bash -c 'rosbag record -O /data/%s __name:=bag_recorder_node" %(computer, submission_id, step_name, computer, filename)
+    cmd = "docker -H %s rm -f bag_recorder || echo not bag_recorder before; cd /home/amaury/AIDO3_experiment_data; mkdir -p submission_%s/%s; docker -H %s run --name bag_recorder --rm -v /home/amaury/AIDO3_experiment_data:/data --net=host -dit duckietown/dt-ros-commons:master19-amd64 /bin/bash -c 'rosbag record -O /data/%s __name:=bag_recorder_node" %(computer, submission_id, step_name, computer, filename)
     for device in device_list:
         cmd += " /"+device+"/imageSparse/compressed /"+device+"/camera_node/camera_info"
     cmd+="'"
