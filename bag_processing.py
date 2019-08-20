232db8c718f8730153a699b31f6d07a2eb4dd023
AmaurX <amaury.camus92@gmail.com>
2019-08-20 17:14:18 +0200

    read the bags

--- bag_processing.py
+++ bag_processing.py
@@ -5,14 +5,17 @@ import docker
 
 
 def merge_bags(bags_name, output_bag_path):
-    # output_bag = rosbag.Bag(output_bag_path, 'w')
-    # for bag_name in bags_name:
-    #     bag = rosbag.Bag(bag_name)
-    #     for topic, message, timestamp in bag.read_messages():
-    #         output_bag.write(topic, message, timestamp)
-    #     bag.close()
-    # output_bag.close()
-    # print("finished with merging the bags")
+    output_bag = rosbag.Bag(output_bag_path, 'w')
+    for bag_name in bags_name:
+        print("trying to do %s" % bag_name)
+        bag = rosbag.Bag(bag_name)
+        for topic, message, timestamp in bag.read_messages():
+            print(topic)
+            output_bag.write(topic, message, timestamp)
+        output_bag.flush()
+        bag.close()
+    output_bag.close()
+    print("finished with merging the bags")
     return ("Success")
 
 
@@ -34,7 +37,7 @@ def start_bag_processing(input_bag_name, output_bag_name, mount_computer_side, m
     container_side_input = "%s/%s" % (mount_container_side, input_bag_name)
     print(watchtowers)
     print(autobots)
-    for watchtower_id in watchtowers:
+    for watchtower_id in watchtowers[0:1]:
         processed_bag_name = "processed_%s.bag" % watchtower_id
         output_container = "%s/%s" % (
             mount_container_side, processed_bag_name)
@@ -55,14 +58,19 @@ def start_bag_processing(input_bag_name, output_bag_name, mount_computer_side, m
             'bind': mount_container_side, 'mode': 'rw'}}
         name = "apriltagprocessor%s" % watchtower_id
         try:
-            client.prune()
+            client.containers.prune()
             container = client.containers.run(
-                image="duckietown/apriltag-processor:master19-amd64", auto_remove=True, detach=True, environment=env, volumes=volume, name=name)
+                image="duckietown/apriltag-processor:master19-amd64", detach=True, environment=env, volumes=volume, name=name)
             print("Success: ")
             print(container.status)
-            while container.status == "running":
-                container.reload()
-                time.sleep(1)
+            while container.status == "running" or container.status == "created":
+                try:
+                    container.reload()
+                    time.sleep(1)
+                except:
+                    break
+            print(container.logs())
+            return ("Success")
         except subprocess.CalledProcessError as e:
             return ("Error: %s" % e)
 
@@ -75,13 +83,32 @@ def start_bag_processing(input_bag_name, output_bag_name, mount_computer_side, m
             mount_container_side, processed_bag_name)
         output_computer = "%s/%s" % (mount_computer_side, processed_bag_name)
         bags_name.append(output_computer)
+        container_side_input = "%s/%s" % (mount_container_side, input_bag_name)
+
         # cmd = "docker rm -f wheelodometryprocessor%s; docker-compose -f processor_compose.yaml run -v %s:%s -e ACQ_DEVICE_NAME=%s -e INPUT_BAG_PATH=%s -e OUTPUT_BAG_PATH=%s --rm --name wheelodometryprocessor%s odometry-processor" % (
         #     autobot, mount_computer_side, mount_container_side, autobot, container_side_input, output_container, autobot)
-        cmd = "docker rm -f apriltagprocessor%s || echo 'blabla'; docker run hello-world " % watchtower_id
+        # cmd = "docker rm -f apriltagprocessor%s || echo 'blabla'; docker run hello-world " % autobot
 
+        env = []
+        env.append("ACQ_DEVICE_NAME=%s" % autobot)
+        env.append("INPUT_BAG_PATH=%s" % container_side_input)
+        env.append("OUTPUT_BAG_PATH=%s" % output_container)
+        env.append("ROS_MASTER_URI=http://172.31.168.115:11311")
+        volume = {mount_computer_side: {
+            'bind': mount_container_side, 'mode': 'rw'}}
+        name = "odometryprocessor%s" % autobot
         try:
-            res = subprocess.check_output(cmd, shell=True)
-            print(str("Success: %s" % res))
+            client.containers.prune()
+            container = client.containers.run(
+                image="duckietown/wheel-odometry-processor:master19-amd64", auto_remove=True, detach=True, environment=env, volumes=volume, name=name)
+            print("Success: ")
+            print(container.status)
+            while container.status == "running" or container.status == "created":
+                try:
+                    container.reload()
+                    time.sleep(1)
+                except:
+                    break
         except subprocess.CalledProcessError as e:
             print("Error: %s" % e)
             return ("Error: %s" % e)
