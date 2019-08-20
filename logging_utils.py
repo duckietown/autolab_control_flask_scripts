fa65dfd1e9ec821f0ed1e6198e6e908237ff7c46
AmaurX <amaury.camus92@gmail.com>
2019-08-20 15:26:57 +0200

    some change

--- logging_utils.py
+++ logging_utils.py
@@ -1,18 +1,25 @@
-import subprocess,time
+import subprocess
+import time
+
 
 def start_logging(computer, filename, device_list, mount_folder):
-    cmd = "docker -H %s rm -f bag_recorder || echo not bag_recorder before;  mkdir -p %s; docker -H %s run --name bag_recorder --rm -v %s:/data --net=host -dit duckietown/dt-ros-commons:master19-amd64 /bin/bash -c 'rosbag record -O /data/%s __name:=bag_recorder_node" %(computer, mount_folder, computer, mount_folder, filename)
+    cmd = "docker -H %s rm -f bag_recorder || echo not bag_recorder before;  mkdir -p %s; docker -H %s run --name bag_recorder --rm -v %s:/data --net=host -dit duckietown/dt-ros-commons:master19-amd64 /bin/bash -c 'rosbag record -O /data/%s __name:=bag_recorder_node" % (
+        computer, mount_folder, computer, mount_folder, filename)
     for device in device_list:
         cmd += " /"+device+"/imageSparse/compressed /"+device+"/camera_node/camera_info"
-    cmd+="'"
+        if "bot" in device:
+            cmd += " /"+device+"/wheels_driver_node/wheels_cmd"
+    cmd += "'"
     try:
         subprocess.check_output(cmd, shell=True)
         return "Success"
     except subprocess.CalledProcessError:
         return "Error"
 
+
 def stop_logging(computer):
-    cmd = "docker -H %s exec bag_recorder /bin/bash -c 'source /opt/ros/kinetic/setup.bash; rosnode kill bag_recorder_node'" % (computer)
+    cmd = "docker -H %s exec bag_recorder /bin/bash -c 'source /opt/ros/kinetic/setup.bash; rosnode kill bag_recorder_node'" % (
+        computer)
 
     try:
         subprocess.check_output(cmd, shell=True)
