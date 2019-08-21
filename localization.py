6a894e915a9e50496b9cb43cffc004b094c98d4c
AmaurX <amaury.camus92@gmail.com>
2019-08-21 18:44:48 +0200

    fix the localization

--- localization.py
+++ localization.py
@@ -1,13 +1,32 @@
 import subprocess
 import rosbag
+import docker
+
+name = "localization-graphoptimizer"
+
 
 def run_localization(input_bag_path, output_dir, mount_computer_side, mount_container_side="/data"):
-
-    cmd = "docker-compose -f processor_compose.yaml run -v %s:%s -e  -e ATMSGS_BAG=%s -e OUTPUT_DIR=%s --name localization-graphoptimizer localization" % (
-        mount_computer_side, mount_container_side, input_bag_path, output_dir)
+    input_bag_path = "processed_apriltags"
+    cmd = "docker-compose -f processor_compose.yaml run --rm -v %s:%s -e  ATMSGS_BAG=%s/%s.bag -e OUTPUT_DIR=%s --name %s localization" % (
+        mount_computer_side, mount_container_side, mount_container_side, input_bag_path, output_dir, name)
+    print(cmd)
     try:
-        res = subprocess.check_output(cmd, shell=True)
-        return ("Success: %s" % res)
+        res = subprocess.Popen(cmd, shell=True)
+        print(res)
+        return ("Success")
     except subprocess.CalledProcessError as e:
         return ("Error: %s" % e)
     pass
+
+
+def check_localization():
+    client = docker.from_env()
+    container_list = client.containers.list()
+    for container in container_list:
+        if container.name == name:
+            status = container.status
+            if status == "running" or status == "created":
+                return("Running")
+            if status == "exited":
+                return("Success")
+    return("Success")
