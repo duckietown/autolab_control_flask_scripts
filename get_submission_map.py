8538876fe15d48e233fc7ded8c213dd62a35f6f4
webbe035 <webbe035@github.com>
2019-09-02 15:34:28 +0200

    Fetching map

--- get_submission_map.py
+++ get_submission_map.py
@@ -1,2 +1,21 @@
+import subprocess
+
 def get_map(container):
-    return "http://duckietown3.local:5050/static/map.svg"
+
+    try:
+        print("Starting the map container: "+container)
+        cmd = "docker run --name map_container -it --rm %s" %(container)
+        subprocess.Popen(cmd, shell=True, executable="/bin/bash")
+
+        cmd = "docker cp map_container:/project/robotarium_scenario_maker/compiled/aido2-LFVI-real-validation/eval0/drawing.svg /static/map.svg"
+        subprocess.check_output(cmd, shell=True, executable="/bin/bash")
+
+        cmd = "docker rm -f map_container"
+        subprocess.Popen(cmd, shell=True, executable="/bin/bash")
+
+        return "/static/map.svg"
+
+    except subprocess.CalledProcessError:
+        return "Error"
+
+    
