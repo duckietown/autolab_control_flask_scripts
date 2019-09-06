f9274344da2fac44eef0fdc3423703f9b6b9e4fc
AmaurX <amaury.camus92@gmail.com>
2019-09-06 13:28:16 +0200

    Added path's to ipfs array

--- ipfs_hash.py
+++ ipfs_hash.py
@@ -1,11 +1,19 @@
 import subprocess
 
 def create_hashes(path):
-    
+
     try:
-        cmd = "ipfs add -r -q %s" % (path)
+        cmd = "ipfs add -r %s" % (path)
         out = subprocess.check_output(cmd, shell=True, executable="/bin/bash")
-        result = out.split('\n')
+        tmp = out.split('\n')
+	if (tmp[-1]==""):
+		tmp = tmp[:-1]
+	result = []
+	for i, line in enumerate(tmp):
+		line=line.split(' ')
+		result.append([])
+		result[i].append(line[1])
+		result[i].append(line[2])
         return result
 
     except subprocess.CalledProcessError:
