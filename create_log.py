7ce993e02d615040b020cf3f52ee793a221fb372
webbe035 <webbe035@github.com>
2019-09-06 17:10:03 +0200

    Debugging

--- create_log.py
+++ create_log.py
@@ -1,7 +1,14 @@
 import yaml
+import subprocess
 
 def generate_log_file(content, filename, mount):
-    f = open(mount+"/"+filename,"w")
-    yaml.safe_dump(content,f,default_flow_style=False, encoding='utf-8', allow_unicode=True)
-    f.close
-    return "Success"
+    try:
+        cmd = "mkdir -p %s" % (mount)
+        subprocess.check_output(cmd, shell=True, executable="/bin/bash")
+        f = open(mount+"/"+filename,"w")
+        yaml.safe_dump(content,f,default_flow_style=False, encoding='utf-8', allow_unicode=True)
+        f.close
+        return "Success"
+
+    except subprocess.CalledProcessError:
+        return "Error"
