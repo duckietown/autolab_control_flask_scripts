689718f53c4dc5de2f10d1baecaf8dbf429fa34d
webbe035 <webbe035@github.com>
2019-09-10 18:23:00 +0200

    debugging

--- localization.py
+++ localization.py
@@ -28,17 +28,17 @@ def check_localization(active_bot, passive_bots, origin_path, destination_path):
             status = container.status
             if status == "running" or status == "created":
                 return("Running")
-            if status == "exited":
-                try:
-                    cmd = "mkdir -p %s" % destination_path
-                    subprocess.check_output(cmd, shell=True)
+            else:
+                continue
+    try:
+        cmd = "mkdir -p %s" % destination_path
+        subprocess.check_output(cmd, shell=True)
 
-                    for i in range(len(passive_bots)):
-                        cmd = "mv %s/%s.yaml %s/passive%s.yaml" % (origin_path,passive_bots[i],destination_path,i+1)
-                        subprocess.check_output(cmd, shell=True)
-                    cmd = "mv %s/%s.yaml %s/active.yaml" % (origin_path,active_bot,destination_path)   
-                    subprocess.check_output(cmd, shell=True)
-                    return("Success")
-                except subprocess.CalledProcessError as e:
-                    return ("Error: %s" % e)
-    return("Success")
+        for i in range(len(passive_bots)):
+            cmd = "mv %s/%s.yaml %s/passive%s.yaml" % (origin_path,passive_bots[i],destination_path,i+1)
+            subprocess.check_output(cmd, shell=True)
+        cmd = "mv %s/%s.yaml %s/active.yaml" % (origin_path,active_bot,destination_path)   
+        subprocess.check_output(cmd, shell=True)
+        return("Success")
+    except subprocess.CalledProcessError as e:
+        return ("Error: %s" % e)
