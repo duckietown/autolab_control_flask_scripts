8ca38f902c3daa91584594a1cabb19f616c4f862
webbe035 <webbe035@github.com>
2019-09-12 18:55:57 +0200

    quotation issue fixed

--- start_passive_bots.py
+++ start_passive_bots.py
@@ -22,9 +22,10 @@ def start_device(device):
                 if res == "true":
                     print(device+ ": Try number "+str(count)+" was successfull, the demo is running")
                     time.sleep(30)
-                    cmd = 'docker -H %s.local exec -it demo_%s\
-                         /bin/bash -c "source ../../devel/setup.bash; rostopic pub /%s/joy_mapper_node/joystick_override duckietown_msgs/BoolStamped\
-                         \"{header: {seq: 0, stamp: {secs: 0, nsecs: 0}, frame_id: \'\'}, data: false}\""' % (device,demo_name,device)
+                    cmd = "docker -H %s.local exec -it demo_%s\
+                         /bin/bash -c \"source ../../devel/setup.bash; rostopic pub /%s/joy_mapper_node/joystick_override duckietown_msgs/BoolStamped\
+                         \'{header: {seq: 0, stamp: {secs: 0, nsecs: 0}}, data: false}\'\"" % (device,demo_name,device)
+                    print(cmd)
                     res = subprocess.Popen(cmd, shell=True, executable="/bin/bash")
                     print(device+ ": Released joystick override")
                     return "Started demo"
